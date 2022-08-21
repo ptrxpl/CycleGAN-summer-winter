e784ebaeec5f336f631184422c9c3219d1c0cb9e
ptrxpl <extezpl@gmail.com>
2022-08-21 15:35:42 +0200

    scripts

---
+++ packages_version.py
@@ -0,0 +1,16 @@
+import sys
+import numpy
+import tensorflow as tf
+import matplotlib
+
+print("Python version: ")
+print(sys.version)
+
+print("Numpy version: ")
+print(numpy.version.version)
+
+print("Tensorflow version: ")
+print(tf.__version__)
+
+print("Matplotlib version: ")
+print(matplotlib.__version__)
